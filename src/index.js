28b6e40b222d6c1cb5fdb58ab733de1570bc8306
chevelenkova <chevelj@mail.ru>
2021-02-05 18:16:44 -0500

    feat : add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-
 exports.min = function min (array) {
-  return 0;
+  
+  if(typeof(array)=='undefined' || array.length ==0) return 0
+ return typeof(Math.min(...array)) == "number" ? Math.min(...array) : 0
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof(array)=='undefined' || array.length ==0) return 0
+  return typeof(Math.max(...array)) == "number" ? Math.max(...array) : 0
+ 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof(array)=='undefined' || array.length ==0) return 0
+ 
+  let a=0;
+  let b=0
+  for(let i=0;i<array.length;i++){b=b+array[i], a=a+1}
+  return typeof(b/a) == "number" ? b/a :0;
+  
+
 }
